cbd40ae7070b89c7f35537e8f0c6133bfb77d9bd
LZezak <leonardokazez@gmail.com>
2026-03-26 16:13:05 -0300

    Add files via upload

---
+++ Nombre y pelicula/Program 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            String pelicula;
+
+            Console.WriteLine("Hola Usuario");
+            Console.Write("¿Cual es tu nombre?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + ". " + "Tu pelicula favorita " + pelicula + " es una porqueria");
+
+
+        }
+    }
+}
