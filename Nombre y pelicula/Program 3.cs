963b66e28abd588da111f1471bfa4d7b50e7aabf
LZezak <leonardokazez@gmail.com>
2026-03-26 16:48:51 -0300

    Add files via upload

---
+++ Nombre y pelicula/Program 3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int año;
+            int edad;
+
+            Console.WriteLine("Hola Usuario");
+            Console.Write("¿En que año naciste?: ");
+            año = Convert.ToInt32(Console.ReadLine());
+            edad = 2025 - año;
+            Console.WriteLine("Si naciste en el año " + año + " tu edad al comenzar el año es de " + edad + " años.");
+
+
+        }
+    }
+}
