8b7016cd9277f0dd855582544ef0373d488dd0c9
LZezak <leonardokazez@gmail.com>
2026-04-07 17:03:41 -0300

    Add files via upload

---
+++ Ejercicos if else/El Control de Peso de la Razor Crest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Peso;
+            float X;
+
+            Console.Write("Ingresar carga de la nave: ");
+            Peso = Convert.ToInt32(Console.ReadLine());
+            X = Peso - 5000;
+
+            if(Peso <= 5000)
+                {
+                Console.WriteLine("Despegue autorizado. Buen viaje, Mando");
+
+            }
+            else
+            {
+                Console.WriteLine("Alerta: Exceso de peso. Debes descargar " + X + "kg para despegar");
+            }
+
+
+        }
+    }
+}
