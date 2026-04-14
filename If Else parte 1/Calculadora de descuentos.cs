e84050c889da2ffb8b7cb169955320c8db32591d
LZezak <leonardokazez@gmail.com>
2026-04-14 17:00:18 -0300

    Add files via upload

---
+++ If Else parte 1/Calculadora de descuentos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingresar monto a pagar: ");
+            double Precio = double.Parse(Console.ReadLine());
+            double Porcentaje = 15;
+
+            if (Precio >= 5000)
+            {
+                double descuento = Precio * Porcentaje / 100;
+                double PrecioFinal = Precio - descuento;
+                Console.WriteLine("Total a pagar: " + PrecioFinal);
+            }
+            else 
+            {
+                Console.WriteLine("Total a pagar: " + Precio);
+            }
+            
+
+        }
+    }
+}
