5dd40fb0bbb9d6b7dbfb245940196b000b0ddb7b
LZezak <leonardokazez@gmail.com>
2026-04-14 16:37:59 -0300

    Add files via upload

---
+++ If Else parte 1/Control de Velocidad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Velocidad del vehiculo (km/h): ");
+            int Respuesta = int.Parse(Console.ReadLine());
+
+            if (Respuesta >= 120)
+            {
+                Console.WriteLine("Multa en camino: Exceso de velocidad");
+            }
+            else 
+            {
+                Console.WriteLine("Conducción segura");
+            }
+            
+
+        }
+    }
+}
