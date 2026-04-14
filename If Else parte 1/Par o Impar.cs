0f0fc67ad3220b45f93e22dfc55918e3d797cdb6
LZezak <leonardokazez@gmail.com>
2026-04-14 16:49:42 -0300

    Add files via upload

---
+++ If Else parte 1/Par o Impar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Escribe un número: ");
+            float Número = float.Parse(Console.ReadLine());
+
+            if (Número % 2 == 0)
+            {
+                Console.WriteLine("Es par.");
+            }
+            else 
+            {
+                Console.WriteLine("Es impar.");
+            }
+            
+
+        }
+    }
+}
