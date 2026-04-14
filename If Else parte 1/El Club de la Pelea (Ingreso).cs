2a1b49309c00fc823b8f36505285ab5aaff24e91
LZezak <leonardokazez@gmail.com>
2026-04-14 16:42:37 -0300

    Add files via upload

---
+++ If Else parte 1/El Club de la Pelea (Ingreso).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nombre: ");
+            string Nombre = Console.ReadLine();
+            Console.Write("Edad: ");
+            int Edad = int.Parse(Console.ReadLine());
+
+            if (Edad >= 18)
+            {
+                Console.WriteLine("Bienvenido al club, " + Nombre);
+            }
+            else 
+            {
+                Console.WriteLine("Lo siento, eres muy polluelo para esto");
+            }
+            
+
+        }
+    }
+}
