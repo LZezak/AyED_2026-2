41e798db70373b4d6e2614523b5352693d9d9328
LZezak <leonardokazez@gmail.com>
2026-04-14 16:54:56 -0300

    Add files via upload

---
+++ If Else parte 1/Aprobación de Examen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingresa tu nota: ");
+            double Nota = double.Parse(Console.ReadLine());
+
+            if (Nota >= 7)
+            {
+                Console.WriteLine("Promocionado");
+            }
+            else if (Nota >= 4)
+            {
+                Console.WriteLine("A finales");
+            }
+            else if (Nota < 4)
+            {
+                Console.WriteLine("Recuperatorio");
+            }
+            
+
+        }
+    }
+}
