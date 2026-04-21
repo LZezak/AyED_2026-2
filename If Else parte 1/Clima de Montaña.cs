ca00a71fa7efeea69a77562987b6715936e5a335
LZezak <leonardokazez@gmail.com>
2026-04-21 14:13:01 -0300

    Add files via upload

---
+++ If Else parte 1/Clima de Montaña.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese temperatura actual: ");
+            int Temperatura = int.Parse(Console.ReadLine());
+
+            if (Temperatura <= 0)
+            {
+                Console.WriteLine("Peligro de congelamiento");
+            }
+            if (Temperatura > 0)
+            {
+                if(Temperatura < 15)
+                Console.WriteLine("Mucho frío");
+            }
+            if(Temperatura > 15)
+            {
+                Console.WriteLine("Clima agradable");
+            }
+
+
+        }
+    }
+}
