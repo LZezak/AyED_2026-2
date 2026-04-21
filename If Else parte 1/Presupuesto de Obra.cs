ad97604c4949c505c4ac80e916c34d385daa2b58
LZezak <leonardokazez@gmail.com>
2026-04-21 14:30:48 -0300

    Add files via upload

---
+++ If Else parte 1/Presupuesto de Obra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese presupuesto de la obra: ");
+            int Presupuesto = int.Parse(Console.ReadLine());
+            Console.Write("Ingresar costo de los materiales: ");
+            int Costo = int.Parse(Console.ReadLine());
+
+            if (Costo > Presupuesto)
+            {
+                int Calculo = Costo - Presupuesto;
+                Console.WriteLine("Se necesitan pedir prestados " + Calculo + "$.");
+            }
+            
+
+        }
+    }
+}
