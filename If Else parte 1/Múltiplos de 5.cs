fdd67672c4d4a060246eac2369367b92751ca08e
LZezak <leonardokazez@gmail.com>
2026-04-21 14:21:15 -0300

    Add files via upload

---
+++ If Else parte 1/Múltiplos de 5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int Número = int.Parse(Console.ReadLine());
+            int Calculo = Número % 5;
+
+            if (Calculo == 0)
+            {
+                Console.WriteLine(Número + " es divisible por 5.");
+            }
+            else
+            {
+                Console.WriteLine(Número + " no es divisible por 5.");
+            }
+
+        }
+    }
+}
