d9d58c5fe6399dd12996cdcc5e2d07efb64ef2ee
LZezak <leonardokazez@gmail.com>
2026-04-21 14:02:04 -0300

    Add files via upload

---
+++ If Else parte 1/Login Simple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese nombre de usuario: ");
+            string Usuario = Console.ReadLine();
+            Console.Write("Ingrese contraseña: ");
+            int Contraseña = int.Parse(Console.ReadLine());
+
+            if (Usuario == "admin")
+            {
+                if (Contraseña == 1234)
+                { 
+                Console.WriteLine("Acceso concedido");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error de credenciales");
+            }
+
+
+        }
+    }
+}
