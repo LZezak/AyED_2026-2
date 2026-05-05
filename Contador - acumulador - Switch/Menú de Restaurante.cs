0387b199f61cfd791137b962dec40c4c19916153
LZezak <leonardokazez@gmail.com>
2026-05-05 16:59:56 -0300

    Add files via upload

---
+++ Contador - acumulador - Switch/Menú de Restaurante.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seleccione un idioma / Select a language /");
+            Console.WriteLine("Sélectionnez une langue / Wählen Sie eine Sprache aus:");
+            Console.WriteLine("(S)ándwich");
+            Console.WriteLine("(P)izza");
+            Console.WriteLine("(H)amburguesa");
+            Console.Write("¿Que va a comer hoy?: ");
+
+            String opción = Console.ReadLine();
+
+            switch (opción)
+            {
+                case "S":
+                    Console.WriteLine("Eligió Sándwich. El precio es de $5.000");
+                    break;
+                case "P":
+                    Console.WriteLine("Eligió Pizza. El precio es de $15.000");
+                    break;
+                case "H":
+                    Console.WriteLine("Eligió Hamburguesa. El precio es de $3.700");
+                    break;
+                default:
+                    Console.WriteLine("Producto no encontrado.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Contador - acumulador - Switch/Registro de Temperaturas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Acumulador = 0;
+            int Contador = 0;
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.Write("Ingrese la temperatura del dia " + i + ": ");
+                int Temp = int.Parse(Console.ReadLine());
+                Acumulador = Acumulador + Temp;
+                if (Temp < 0)
+                {
+                    Contador = Contador + 1;
+                }
+
+            }
+            Console.WriteLine("La temperatura sumada fue: " + Acumulador + " grados.");
+            Console.WriteLine("La temperatura fue menor a 0 grados un total de " + Contador + " veces.");
+
+        }
+    }
+}
