41ecc64a8aab1e9029b6961b1afb8f0f8401ecfc
LZezak <leonardokazez@gmail.com>
2026-05-07 14:33:15 -0300

    Add files via upload

---
+++ Contador - acumulador - Switch/Gestión de Gastos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Contador = 0;
+            Console.Write("Ingrese un número entero positivo: ");
+            int Numero = int.Parse(Console.ReadLine());
+            for (int i = 2; i == Numero - 1; i++)
+            {
+                int Calculo = Numero % i;
+                if (Calculo == 0)
+                {
+                    Contador = Contador + 1;
+                }
+            }
+            if (Contador >= 2)
+            {
+                Console.WriteLine("No es primo");
+            }
+            else
+            {
+                Console.WriteLine("Es primo");
+
+            }
+            Console.WriteLine("Contador = " + Contador);
+        }
+    }
+}
