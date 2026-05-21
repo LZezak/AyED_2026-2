161fb7e84f23375da3c9d50469ec3ffdac357770
LZezak <leonardokazez@gmail.com>
2026-05-21 14:16:23 -0300

    Add files via upload

---
+++ Contador - acumulador - Switch/Guia While  Do-While #1/Multiplicación por sumas sucesivas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese valor de A: ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese valor de B: ");
+            int B = int.Parse(Console.ReadLine());
+            int Resultado = 0;
+            Console.Write(A + " x " + B + " = ");
+            while (A != 0)
+            {
+                Resultado = Resultado + B;
+                A--;
+            }
+            Console.WriteLine(Resultado);
+
+        }
+    }
+}
---
+++ Contador - acumulador - Switch/Guia While  Do-While #1/Validación de opción de menú básica.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eleccion;
+            do
+            {
+                Console.WriteLine("Elija una opción:");
+                Console.WriteLine("1. Saludar");
+                Console.WriteLine("2. Despedirse");
+                Console.WriteLine("3. Salir");
+                Console.Write("Elija: ");
+                eleccion = int.Parse(Console.ReadLine());
+                if (eleccion == 1)
+                {
+                    Console.WriteLine("Hola");
+                }
+                else if (eleccion == 2)
+                {
+                    Console.WriteLine("Adios");
+                }
+                else
+                {
+                    Console.WriteLine("La respuesta ingresada no es valida");
+                    Console.WriteLine("Intente de nuevo");
+                }
+
+            } while (eleccion != 3);
+            
+        }
+    }
+}
