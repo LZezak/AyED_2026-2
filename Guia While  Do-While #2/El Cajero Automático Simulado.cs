8cb6f0d1616fd08c8aecc7f4cc9334432597b94d
LZezak <leonardokazez@gmail.com>
2026-05-21 16:44:57 -0300

    Add files via upload

---
+++ Guia While  Do-While #2/El Cajero Automático Simulado.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Saldo = 10000;
+            int Eleccion;
+            do
+            {
+                Console.WriteLine("Seleccione que desea hacer: ");
+                Console.WriteLine("1. Depositar dinero");
+                Console.WriteLine("2. Retirar dinero");
+                Console.WriteLine("3. Ver saldo actual");
+                Console.WriteLine("4. Salir");
+                Console.Write("Elija: ");
+                Eleccion = int.Parse(Console.ReadLine());
+                switch (Eleccion)
+                {
+                    case 1:
+                        Console.Write("Cuanto dinero desea ingresar: ");
+                        int DineroIngresado = int.Parse(Console.ReadLine());
+                        Saldo = Saldo + DineroIngresado;
+                        Console.WriteLine("$" + DineroIngresado + " ingresados con exito");
+                        Console.WriteLine("Saldo actual de: $" + Saldo);
+                        break;
+
+                    case 2:
+                        Console.Write("Cuanto dinero desea retirar: ");
+                        int DineroRetirado = int.Parse(Console.ReadLine());
+                        Saldo = Saldo - DineroRetirado;
+                        Console.Write("$" + DineroRetirado + " retirados con exito");
+                        Console.WriteLine("Saldo actual de: $" + Saldo);
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Saldo actual: $" + Saldo);
+                        break;
+
+                    case 4:
+                        Eleccion = 0;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion no valida, intente de nuevo");
+                        break;
+                }
+
+            } while (Eleccion != 0) ;
+            
+        }
+    }
+}
---
+++ Guia While  Do-While #2/El juego de Adivina el Número con límites.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumSecreto = 37;
+            Console.WriteLine("ADIVINE EL NÚMERO (1-100)");
+            Console.WriteLine("5 INTENTOS");
+            int Eleccion;
+            int Bandera = 1;
+            int ContadorFallos = 0;
+            do
+            {
+                Console.Write("Cual cree que es el número: ");
+                Eleccion = int.Parse(Console.ReadLine());
+                if (Eleccion != NumSecreto)
+                {
+                    ContadorFallos++;
+                    if(Eleccion < NumSecreto)
+                    {
+                        Console.WriteLine("El número es mas grande");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El número es mas pequeño");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Felicidades, hallaste el número secreto en " + (ContadorFallos + 1) + " intento/s");
+                    Bandera = 0;
+                }
+
+                if(ContadorFallos == 5)
+                {
+                    Console.WriteLine("Pero que pena, 5 intentos. Perdiste.");
+                    Bandera = 0;
+                }
+
+            } while (Bandera != 0);
+
+        }
+    }
+}
