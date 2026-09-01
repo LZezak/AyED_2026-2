5bcecdba1eea2e26fafe75c98b6c731dd924749d
LZezak <leonardokazez@gmail.com>
2026-09-01 14:43:21 -0300

    Add files via upload

---
+++ Funciones - Primeros pasos/Factorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            Console.Write("Ingrese el número para sacar su facorial: ");
+            Numero = int.Parse(Console.ReadLine());
+            Factorial(Numero);
+        }
+        static void Factorial(int Num)
+        {
+            int Total = 0;
+            int NumMensaje = Num;
+            Total = Num * (Num - 1);
+            Num = Num - 2;
+            while (Num != 0)
+            {
+                Total = Total * (Num);
+                Num--;
+            }
+            Console.WriteLine("El resultado del factorial de " + NumMensaje + " es: " + Total);
+        }
+    }
+}
---
+++ Funciones - Primeros pasos/Potencia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el la base: ");
+            int Base = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el la base (mayor o igual a 0): ");
+            int Exponente = int.Parse(Console.ReadLine());
+            if (Exponente < 0)
+            {
+                while (Exponente < 0)
+                {
+                    Console.Write("El numero ingresado es menor a 0, intente de nuevo: ");
+                    Exponente = int.Parse(Console.ReadLine());
+                }
+            }
+            Potencia(Base, Exponente);
+        }
+        static void Potencia(int Bas, int Expo)
+        {
+            int ExpoMen = Expo;
+            int Total = Bas * Bas;
+            Expo = Expo - 2;
+            while (Expo != 0)
+            {
+                Total = Total * Bas;
+                Expo--;
+            }
+            Console.WriteLine("El la potencia de " + Bas + " elevado a " + ExpoMen + " es: " + Total);
+        }
+    }
+}
